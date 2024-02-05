a5b90931e3ab083f235e2d6a0a4fa8ad920f3bb2
Trond Erling <trond.erling@gmail.com>
2024-02-05 19:50:54 +0100

    Prevent multiple event listeners to be registered

--- mf-host/src/useNavigationEvents.tsx
+++ mf-host/src/useNavigationEvents.tsx
@@ -1,9 +1,16 @@
 import { Router } from "@remix-run/router";
+import { useEffect } from "react";
 
+let hasAddedListener = false; // Would not work for SSR / could use a ref instead, but this is a demo.. so..
 export default function useNavigationEvents(router: Router) {
-    // @ts-expect-error Customevent is not in the types and I am too lazy to add a CustomEvent type.
-    window.addEventListener("navigate", (event: CustomEvent) => {
-        const { to } = event?.detail ?? {};
-        router.navigate(to);
+    useEffect(() => {
+        if (hasAddedListener) return; // forcing the event listener to only be registered once.
+
+        // @ts-expect-error Customevent is not in the types and I am too lazy to add a CustomEvent type.
+        window.addEventListener("navigate", (event: CustomEvent) => {
+            const { to } = event?.detail ?? {};
+            router.navigate(to);
+        });
+        hasAddedListener = true; // Flag to prevent adding the event listener more than once.
     });
 }
